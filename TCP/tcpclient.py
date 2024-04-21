26b165b2d04c578b1df4ca5494884f59941e277d
Guilherme Argilar <guilherme.argilar@gmail.com>
2024-04-21 19:51:05 -0300

    Refactor TCP client code to handle file transfers and improve user experience

--- TCP/tcpclient.py
+++ TCP/tcpclient.py
@@ -2,29 +2,48 @@ import socket
 import threading
 import os
 
-# Variável global para armazenar o nickname do usuário
+# Variável global para armazenar o nickname do usuário e o estado de execução
 user_nickname = None
-
+is_running = True
 
 def receive_messages(client_socket):
-    while True:
-        try:
+    global is_running
+    try:
+        while is_running:
             data = client_socket.recv(2048)
+            if not data:
+                break
             if data.startswith(b'/FILE'):
                 handle_file_reception(data, client_socket)
             else:
                 print(data.decode('utf-8'))
-        except:
-            print("Erro ao receber dados do servidor.")
-            client_socket.close()
-            break
+    except Exception as e:
+        if is_running:
+            print(f"Erro ao receber dados: {e}")
+    finally:
+        client_socket.close()
+        print("Conexão encerrada pelo servidor.")
 
+def close_client(client_socket):
+    global is_running
+    is_running = False
+    try:
+        client_socket.send("/QUIT".encode('utf-8'))
+        client_socket.shutdown(socket.SHUT_RDWR)
+    except Exception as e:
+        print(f"Erro ao enviar comando de desconexão: {e}")
+    client_socket.close()
+    print("Cliente fechado com sucesso.")
 
 def handle_user_input(client_socket):
+    global is_running
     while True:
         message = input("-> ")
         if message.startswith("/"):
-            if message.strip() == "/LIST":
+            if message.startswith("/QUIT"):
+                print("Saindo do chat.")
+                break
+            elif message.strip() == "/LIST":
                 client_socket.send("/LIST".encode('utf-8'))
             elif message.startswith("/SEND"):
                 send_file(client_socket, message)
@@ -34,28 +53,22 @@ def handle_user_input(client_socket):
                 process_command(client_socket, message)
         else:
             print('comando desconhecido')
-            
+    close_client(client_socket)
+
 def process_command(client_socket, command):
     parts = command.split(maxsplit=2)
-    if command.startswith("/QUIT"):
-        print("Saindo do chat.")
-        client_socket.close()
-        exit()
-    elif command.startswith("/MSG") and len(parts) > 2:
+    if command.startswith("/MSG") and len(parts) > 2:
         send_private_message(client_socket, parts[1], parts[2])
     else:
         print("Comando desconhecido.")
-
 
 def send_message(client_socket, message):
     formatted_message = f"{user_nickname}: {message}"
     client_socket.send(formatted_message.encode('utf-8'))
 
-
 def send_private_message(client_socket, recipient, message):
-    formatted_message = f"/MSG {recipient}  {message}"
+    formatted_message = f"/MSG {recipient} {message}"
     client_socket.send(formatted_message.encode('utf-8'))
-
 
 def send_file(client_socket, command):
     parts = command.split()
@@ -65,22 +78,16 @@ def send_file(client_socket, command):
         try:
             with open(file_path, 'rb') as file:
                 file_data = file.read()
-                # Preparar a mensagem de arquivo com uma tag de identificação
-                header = f'/SEND {recipient} {
-                    os.path.basename(file_path)}'.encode('utf-8')
+                header = f'/SEND {recipient} {os.path.basename(file_path)}'.encode('utf-8')
                 client_socket.send(header + b' ' + file_data)
         except FileNotFoundError:
             print("Arquivo não encontrado.")
-
-
-
 
 def handle_file_reception(data, client_socket):
     _, filename, file_data = data.split(b' ', 2)
     with open(filename.decode('utf-8'), 'wb') as file:
         file.write(file_data)
     print(f"Arquivo {filename.decode('utf-8')} recebido.")
-
 
 def register_user(client_socket):
     global user_nickname
@@ -93,7 +100,6 @@ def register_user(client_socket):
         else:
             print("Por favor, insira um apelido válido.")
 
-
 def list_commands():
     print("Comandos disponíveis:")
     print("Digite uma mensagem e pressione enter para enviar para todos os usuários da sala.")
@@ -102,7 +108,6 @@ def list_commands():
     print('/S <mensagem> - Envia uma mensagem para todos os usuários da sala.')
     print("/SEND <usuario> <caminho_do_arquivo> - Envia um arquivo para o usuário especificado.")
     print("/QUIT - Sai do chat.")
-
 
 def main():
     server_host = '127.0.0.1'
@@ -113,13 +118,11 @@ def main():
         print("Conectado ao servidor!")
         register_user(client_socket)
         list_commands()
-        threading.Thread(target=receive_messages,
-                         args=(client_socket,)).start()
+        threading.Thread(target=receive_messages, args=(client_socket,)).start()
         handle_user_input(client_socket)
     except Exception as e:
         print(f"Não foi possível conectar ao servidor: {e}")
         client_socket.close()
-
 
 if __name__ == "__main__":
     main()
--- TCP/tcpserver.py
+++ TCP/tcpserver.py
@@ -1,53 +1,50 @@
-
 import socket
 import threading
-
 
 def client_thread(conn, all_connections, all_nicknames):
     while True:
         try:
             data = conn.recv(2048)
-            if data:
-                if data.startswith(b'/SEND'):
-                    handle_file_sending(data, all_nicknames)
-                elif data.decode().startswith("/LIST"):
-                    list_message = ", ".join(all_nicknames.keys())
-                    conn.send(f"Users connected: {
-                              list_message}".encode('utf-8'))
-                elif data.decode().startswith("/REG"):
-                    nickname = data.decode().split(' ')[1]
-                    all_nicknames[nickname] = conn
-                elif data.decode().startswith("/MSG"):
-                    parts = data.decode().split(' ', 2)
-                    if len(parts) > 2:
-                        sender = [nick for nick,
-                                  c in all_nicknames.items() if c == conn][0]
-                        recipient = parts[1]
-                        message = parts[2]
-                        recipient_conn = all_nicknames.get(recipient)
-                        if recipient_conn:
-                            formatted_message = f"Msg privada de {
-                                sender}: {message}"
-                            recipient_conn.send(
-                                formatted_message.encode('utf-8'))
-                else:
-                    broadcast_message(data, conn, all_connections)
+            client_address = conn.getpeername()  # Obtém o endereço do cliente
+            if not data:
+                raise Exception("Client disconnected")
+            print(f"Dado recebido de {client_address}: {data}")
+            
+            if data.startswith(b'/SEND'):
+                handle_file_sending(data, all_nicknames)
+            elif data.decode().startswith("/LIST"):
+                list_message = ", ".join(all_nicknames.keys())
+                conn.send(f"Users connected: {list_message}".encode('utf-8'))
+            elif data.decode().startswith("/REG"):
+                nickname = data.decode().split(' ')[1]
+                all_nicknames[nickname] = conn
+            elif data.decode().startswith("/QUIT"):
+                raise Exception("Client requested disconnect")
+            elif data.decode().startswith("/MSG"):
+                parts = data.decode().split(' ', 2)
+                if len(parts) > 2:
+                    sender = [nick for nick, c in all_nicknames.items() if c == conn][0]
+                    recipient, message = parts[1], parts[2]
+                    recipient_conn = all_nicknames.get(recipient)
+                    if recipient_conn:
+                        formatted_message = f"Private msg from {sender}: {message}"
+                        recipient_conn.send(formatted_message.encode('utf-8'))
+            else:
+                broadcast_message(data, conn, all_connections)
         except Exception as e:
-            print(f"Error: {e}")
-            conn.close()
-            remove_client(conn, all_nicknames, all_connections)
-
+            print(f"Error from {client_address}: {e}")
+            break
+    remove_client(conn, all_nicknames, all_connections)
 
 def broadcast_message(message, sender_conn, all_connections):
     decoded_message = message.decode('utf-8')
-    command_start_index = decoded_message.find("/S")
-    message_content = decoded_message[:command_start_index] + \
-        decoded_message[command_start_index + 3:]
-    encoded_message = message_content.encode('utf-8')
-    for client_conn in all_connections:
-        if client_conn != sender_conn:
-            client_conn.send(encoded_message)
-
+    command_start_index = decoded_message.find("/S ")
+    if command_start_index != -1:
+        message_content = decoded_message[command_start_index + 3:]
+        encoded_message = message_content.encode('utf-8')
+        for client_conn in all_connections:
+            if client_conn != sender_conn:
+                client_conn.send(encoded_message)
 
 def handle_file_sending(data, all_nicknames):
     _, recipient, filename = data.split(b' ', 2)
@@ -55,14 +52,13 @@ def handle_file_sending(data, all_nicknames):
     if recipient_conn:
         recipient_conn.send(b'/FILE ' + filename)
 
-
 def remove_client(conn, all_nicknames, all_connections):
-    nickname_to_remove = [nick for nick,
-                          c in all_nicknames.items() if c == conn]
+    nickname_to_remove = [nick for nick, c in all_nicknames.items() if c == conn]
     if nickname_to_remove:
         del all_nicknames[nickname_to_remove[0]]
     all_connections.remove(conn)
-
+    conn.close()
+    print(f"Connection closed and user {nickname_to_remove[0] if nickname_to_remove else 'unknown'} removed.")
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -70,12 +66,12 @@ def start_server():
     server_socket.listen()
     all_connections = []
     all_nicknames = {}
+    print("Server started and listening on port 5555")
     while True:
         client_conn, client_addr = server_socket.accept()
         all_connections.append(client_conn)
-        threading.Thread(target=client_thread, args=(
-            client_conn, all_connections, all_nicknames)).start()
-
+        print(f"New connection from {client_addr}")
+        threading.Thread(target=client_thread, args=(client_conn, all_connections, all_nicknames)).start()
 
 if __name__ == "__main__":
     start_server()
